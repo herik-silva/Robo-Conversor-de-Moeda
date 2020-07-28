cbf6190157b7cc75cd41b27cb0c911159542e43f
herik-silva <herik.silva.019@gmail.com>
2020-07-28 15:04:57 -0300

    Otimizado a pesquisa dos valores das moedas

--- src/robo.js
+++ src/robo.js
@@ -1,3 +1,5 @@
+const puppeteer = require('puppeteer');
+
 const robo = {
     input: require('./robos/input.js'),
     pesquisar: require('./robos/pesquisar.js'),
@@ -5,12 +7,21 @@ const robo = {
     conversao: require('./robos/conversao.js')
 }
 
+var navegador={
+    browser: null,
+    page: null
+};
+
 async function inciarRobo(){
     var opcao =  robo.input();
     
     switch(opcao){
         case "0":
             console.log("Obrigado por utilizar o Robo Conversor de Moeda");
+            if(navegador.browser != null){
+                console.log(typeof navegador.browser);
+                navegador.browser.close();
+            }
             break;
         case "1":
             await iniciarConversao("real","dolar").catch("Tempo limite expirado!");
@@ -26,8 +37,9 @@ async function inciarRobo(){
 }
 
 async function iniciarConversao(moedaBase, moedaFinal){
-    const valor_moeda_final = await robo.pesquisar(moedaBase,moedaFinal);
-
+    const pesquisa = await robo.pesquisar(moedaBase,moedaFinal,navegador);
+    const valor_moeda_final = pesquisa[0];
+    navegador = pesquisa[1];
     console.log("------ Conversão de Moeda ------");
     console.log(`Atualmente 1 ${moedaBase} vale ${valor_moeda_final} em ${moedaFinal}`);
     console.log("--------------------------------");
